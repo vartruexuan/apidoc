fcd45b87f9c8dc8a6370442751253b96fb91101f
guozhaoxuan <1175398345@qq.com>
2018-07-13 00:22:13 +0800

    删除下文件

--- apidoc/covert/js/main.js
+++
@@ -1,6 +0,0 @@
-//********* 主入口js main,js********//
-
-$(function () {
-    //生成文档
-    //window.ApiHelper.covert();
-})
--- apidoc/dist/zjs/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by PhpStorm.
- * User: 小昭昭
- * Date: 2018/7/7
- * Time: 18:34
- */
-$(function () {
-    window.DistHelper.get_module();
-})
